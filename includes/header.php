3fdfe45d3a0af4965b011cc761ae554132c0cc30
S.M. Al-Amin <smalamin754@gmail.com>
2026-08-02 13:07:45 +0600

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<header class="bg-red-700 shadow-lg mb-4">
+    <div class="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between text-white">
+        <h1 class="text-2xl font-bold">🩸 Blood Bank Portal</h1>
+        <a href="index.php" class="text-white hover:underline">Home</a>
+    </div>
+</header>
